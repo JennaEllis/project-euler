00d00196a193dcb2f65b3ebb35d9a5043c1d018a
Jenna Ellis <ellis93@purdue.edu>
2017-11-17 15:23:29 -0500

    started problem three

---
+++ ProblemThree.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+//PROBLEM DESCRIPTION
+/* "The prime factors of 13195 are 5, 7, 13, and 29. 
+*	 What is the largest prime factor of 
+*	 600851475143?
+*/
+
+public class ProblemThree {
+		public static void main(String args[]){
+			System.out.println("PROBLEM 3: "); 
+
+			
+		}
+}
