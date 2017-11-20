a45da765cba84b4203c5c6ea12223257a6c23084
Jenna Ellis <ellis93@purdue.edu>
2017-11-20 17:15:31 -0500

    completed problem three

--- ProblemThree.java
+++ ProblemThree.java
@@ -10,6 +10,30 @@ public class ProblemThree {
 		public static void main(String args[]){
 			System.out.println("PROBLEM 3: "); 
 
+			Scanner input = new Scanner(System.in); 
+			long number; 
+			System.out.print("Enter a number: "); 
+			number = input.nextLong(); 
+			System.out.println(); 
+
+			int divisor_store = 2; 
+			int largest_divisor_store = 1; 
+
+			while( number != 1 ){
+				if( number % divisor_store == 0){
+					number = number/divisor_store; 
+					if( divisor_store > largest_divisor_store){
+						largest_divisor_store = divisor_store; 
+					}
+					divisor_store = 2; 
+				}
+				else{
+					divisor_store++; 	
+				}
+			} 
+	
+			System.out.println("The largest prime factor of "+ number + " is: " + largest_divisor_store); 
+			
 			
 		}
 }
